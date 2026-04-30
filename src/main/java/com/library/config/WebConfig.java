9c9d9a42144d53b7f4c2fd2472b5497a787eeb1b
HardikSinghal896 <hardiksinghal896@gmail.com>
2026-04-30 15:04:27 +0530

    first commit

--- src/main/java/com/library/config/WebConfig.java
+++ src/main/java/com/library/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.library.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${FRONTEND_URL:http://localhost:5173}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000","http://192.168.29.73:5173","http://49.36.236.223:5173")
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        frontendUrl,
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "http://192.168.29.73:5173/"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
